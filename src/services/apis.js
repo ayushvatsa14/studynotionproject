deb5b6e8554c4be03a444cd176044e495840a909
ayushvatsa14 <ayushvatsa14@gmail.com>
2025-08-14 01:41:51 +0530

    fix

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+//const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "studynotion-backend-eight.vercel.app"
 
 // AUTH ENDPOINTS
 export const endpoints = {
