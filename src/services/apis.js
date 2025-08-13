5a40f9a30420c7f5d32624e74a7f070e4023055b
ayushvatsa14 <ayushvatsa14@gmail.com>
2025-08-14 02:05:39 +0530

    dashboard update

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 //const BASE_URL = "http://localhost:4000/api/v1"
-const BASE_URL = "studynotionproject-backend.vercel.app"
+const BASE_URL = "https://studynotionproject-backend-dqs1.onrender.com"
 
 // AUTH ENDPOINTS
 export const endpoints = {
