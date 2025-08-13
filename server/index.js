35a8a1900926733b479fc1ffa700bae7530ba2ef
ayushvatsa14 <ayushvatsa14@gmail.com>
2025-08-14 02:25:48 +0530

    cors fix

--- server/index.js
+++ server/index.js
@@ -23,7 +23,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		//origin:"http://localhost:3000",
+		origin:"https://studynotionproject-iota.vercel.app",
 		credentials:true,
 	})
 )
